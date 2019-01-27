7d735a62265b0ac7166e7d848089eeb7d58704a7
admnst <13253495869@163.com>
2019-01-28 00:39:12 +0800

    a+edit b create two input for a and b,use the split(',') make array

--- create.php
+++ create.php
@@ -1,41 +1,27 @@
 <?php
 header('Access-Control-Allow-Origin:*');
 
-// if (empty($_GET["a"]) || empty($_GET["b"])) {
-//     echo "empty";
-//     exit;
-// }
-// if (!is_array($_GET["b"])) {
-//     echo "b should be an Array";
-//     exit;
-// }
-// if (strstr($_GET["a"], '$') || strstr($_GET["b"], '$')) {
-//     echo "pleace not type $";
-//     exit;
-// }
-
-// $manager = new MongoDB\Driver\Manager("mongodb://localhost:27017");
-// $bulk = new MongoDB\Driver\BulkWrite;
-// $document = [
-//     '_id' => new MongoDB\BSON\ObjectID,
-//     'a' => $_GET["a"],
-//     'b' => $_GET["b"]
-// ];
-
 $a = $_GET["a"];
+$b = empty($_GET["b"]) ? [] : json_decode($_GET["b"]);
 if (empty($a)) {
-    echo 'a can not be empty';
+    echo "empty";
     exit;
 }
-if (strstr($a, '$')) {
-    echo "please not type $";
+if (!is_array($b)) {
+    echo "b should be an Array";
     exit;
 }
+if (strstr($a, '$') || strstr(json_encode($b), '$')) {
+    echo "pleace not type $";
+    exit;
+}
+
 $manager = new MongoDB\Driver\Manager("mongodb://localhost:27017");
 $bulk = new MongoDB\Driver\BulkWrite;
 $document = [
     '_id' => new MongoDB\BSON\ObjectID,
-    'a' => $_GET["a"]
+    'a' => $a,
+    'b' => $b
 ];
 
 $_id = $bulk->insert($document);
