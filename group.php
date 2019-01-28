bbb169ded0b7e9d3efd992180a5eb3f31ef31308
vision <13253495869@163.com>
2019-01-28 16:00:25 +0800

    group.php but not in viewport

---
+++ group.php
@@ -0,0 +1,43 @@
+<?php
+$manager = new MongoDB\Driver\Manager("mongodb://localhost:27017");
+$cmd = new MongoDB\Driver\Command(
+    [
+        'aggregate' => 'a',
+        'pipeline' => [
+            [
+                '$match' => [
+                    'a' => ['$type' => 2]
+                ]
+            ],
+            [
+                '$group' => [
+                    '_id' => '$a',
+                    'count' => ['$sum' => 1]
+                ]
+            ],
+            [
+                '$sort' => ['count' => -1]
+            ],
+            [
+                '$limit' => 100
+            ],
+            [
+                '$skip' => 0
+            ]
+        ],
+        'cursor' => new stdClass,
+    ]
+);
+try {
+    $cursor = $manager->executeCommand('adb', $cmd)->toArray();
+    $arr = [];
+    foreach ($cursor as $document) {
+        $bson = MongoDB\BSON\fromPHP($document);
+        $arr[] = json_decode(MongoDB\BSON\toJSON($bson));
+    }
+    echo json_encode($arr);
+} catch (MongoDB\Driver\Exception $e) {
+    echo $e->getMessage(), "\n";
+    exit;
+}
+?>
