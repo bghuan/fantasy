679cfc9ac5e1b640b13d8444389460a1bc03ca4c
vision <13253495869@163.com>
2019-01-29 17:24:54 +0800

    static

--- read.php
+++ read.php
@@ -1,27 +1,47 @@
 <?php
 header('Access-Control-Allow-Origin:*');
 $a = $_GET["a"];
+$_id = $_GET["id"];
+$limit = !empty($_GET["limit"]) && preg_match("/^[\d0-9]{1,3}+$/", $_GET["limit"]) ? (int)$_GET["limit"] : 100;
+$skip = !empty($_GET["skip"]) && preg_match("/^[\d0-9]{1,9}+$/", $_GET["skip"]) ? (int)$_GET["skip"] : 0;
 $manager = new MongoDB\Driver\Manager("mongodb://localhost:27017");
-if (empty($a)) {
+if (!empty($_id)) {
+    $_ids = explode(',', trim($_id, '[]'));
+    $arr = [];
+    foreach ($_ids as $_id) {
+        if (preg_match("/^[\d0-9a-f]{24}+$/", $_id)) {
+            $arr[] = new MongoDB\BSON\ObjectID($_id);
+        }
+    }
+    $filter = ['_id' => ['$in' => $arr]];
+    $options = ['sort' => ['_id' => -1]];
+    $query = new MongoDB\Driver\Query($filter, $options);
+    echo json_encode($manager->executeQuery('adb.a', $query)->toArray());
+    exit;
+} else if (!empty($a)) {
+    $filter = ['a' => $a];
+    $options = ['projection' => ['a' => 1, 'b' => 1], 'sort' => ['_id' => -1], 'limit' => $limit, 'skip' => $limit * $skip];
+    $query = new MongoDB\Driver\Query($filter, $options);
+    echo json_encode($manager->executeQuery('adb.a', $query)->toArray());
+    exit;
+} else {
     $cmd = new MongoDB\Driver\Command([
         'aggregate' => 'a',
         'pipeline' => [
-            ['$match' => ['a' => ['$type' => 2]]],
-            ['$group' => ['_id' => '$a', 'id' => ['$min' => '$_id'], 'b' => ['$addToSet' => '$b'], 'count' => ['$sum' => 1]]],
+            ['$match' => ['a' => ['$exists' => true]]],
+            ['$group' => ['_id' => '$a', 'id' => ['$first' => '$_id'], 'b' => ['$addToSet' => '$b'], 'count' => ['$sum' => 1]]],
             ['$sort' => ['count' => -1, '_id' => -1]],
-            ['$limit' => 100]
+            ['$limit' => $limit],
+            ['$skip' => $limit * $skip]
         ],
         'cursor' => new stdClass,
     ]);
     try {
         echo json_encode($manager->executeCommand('adb', $cmd)->toArray());
+        exit;
     } catch (MongoDB\Driver\Exception $e) {
         echo $e->getMessage(), "\n";
+        exit;
     }
-} else {
-    $filter = ['a' => $a];
-    $options = ['projection' => ['a' => 1, 'b' => 1], 'sort' => ['_id' => -1], 'limit' => 100];
-    $query = new MongoDB\Driver\Query($filter, $options);
-    echo json_encode($manager->executeQuery('adb.a', $query)->toArray());
 }
 ?>
