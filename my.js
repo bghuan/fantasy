aa286864d37729b91c1aba1d5d33d777b9d9d8ac
vision <13253495869@163.com>
2019-03-08 13:47:39 +0800

    active use es6 whether good

--- my.js
+++ my.js
@@ -1,19 +1,11 @@
 var a, b;
-var getMyDate = function () {
-    var date = new Date();
-    return (date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate()).toString();
-}
-var HttpGet = function (Url, CallBack) {
-    var xmlhttp;
-    if (window.XMLHttpRequest) {
-        xmlhttp = new XMLHttpRequest();
-    }
-    else {
-        xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
-    }
-    xmlhttp.onreadystatechange = function () {
+const getMyDate = (date = new Date()) => (date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate()).toString();
+
+const HttpGet = (Url, CallBack) => {
+    let xmlhttp = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject("Microsoft.XMLHTTP");
+    xmlhttp.onreadystatechange = () => {
         if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-            var json;
+            let json;
             try {
                 json = JSON.parse(xmlhttp.responseText);
                 console.log(json);
@@ -21,55 +13,57 @@ var HttpGet = function (Url, CallBack) {
             } catch (e) {
                 CallBack(xmlhttp.responseText);
                 console.log(xmlhttp.responseText + '\n' + e)
-            } finally { totop() }
+            } finally {
+                totop()
+            }
         }
     }
     xmlhttp.open("GET", Url, true);
     xmlhttp.send();
 }
-var func_query = function (json) {
+const func_query = (json) => {
     try {
         document.getElementById("a_top").innerHTML = a || 'fantasy';
         document.getElementById("a").value = a || 'fantasy';
         if (a != '' && a != undefined) {
-            var str = '/&nbsp;<a onclick="query(\'' + a + '\');">' + a.substring(0, 5) + '</a>&nbsp;';
-            var str_as = document.getElementById("as").innerHTML;
+            let str = '/&nbsp;<a onclick="query(\'' + a + '\');">' + a.substring(0, 5) + '</a>&nbsp;';
+            let str_as = document.getElementById("as").innerHTML;
             document.getElementById("as").innerHTML = str_as.substring(0, str_as.indexOf(str) >= 0 ? str_as.indexOf(str) : 999);
             document.getElementById("as").innerHTML += str;
         }
         else {
             document.getElementById("as").innerHTML = '<a onclick="query()" style="margin-left:-15px;">&nbsp;&nbsp;&nbsp;</a><a class="float-right text-dark">' + getMyDate() + '</a></div>';
         }
-        var div_query = document.getElementById("div_query");
+        let div_query = document.getElementById("div_query");
         div_query.innerHTML = '';
         for (j in json) {
-            var div = document.createElement("div");
-            var aa = document.createElement("a");
+            let div = document.createElement("div");
+            let aa = document.createElement("a");
             div.className = "navbar-brand col-12 text-truncate border-bottom";
             aa.onclick = function () { query(this.innerHTML) };
             aa.innerHTML = json[j]['a'] || '';
             div.id = json[j]['_id']['$oid'];
             div.appendChild(aa);
-            var b = document.createElement("a");
+            let b = document.createElement("a");
             b.style = "font-size:80%";
             b.innerHTML = json[j]['b'] == null ? '' : ' - ' + json[j]['b'];
             div.appendChild(b);
-            var sum = document.createElement("a");
+            let sum = document.createElement("a");
             sum.className = "float-right";
             sum.innerHTML = json[j]['count'] || '';
             div.appendChild(sum);
-            var star = document.createElement("img");
+            let star = document.createElement("img");
             star.className = "float-right";
             star.src = 'static/svg/s.svg';
             star.width = "15";
-            star.style = "margin:10px 5px;"
+            star.style = "margin:10px 5px;";
             star.onclick = function () { create(this) };
             div.appendChild(star);
             div_query.appendChild(div);
         }
-        var ida = document.getElementsByTagName("img");
-        var str_id = localStorage.getItem('id') || '';
-        for (i in ida) {
+        let ida = document.getElementsByTagName("img");
+        let str_id = localStorage.getItem('id') || '';
+        for (let i in ida) {
             if (ida[i].parentNode != undefined && ida[i].parentNode.id != undefined && ida[i].parentNode.id != '' && str_id.indexOf(ida[i].parentNode.id) >= 0) {
                 ida[i].src = "static/svg/star.svg";
             }
@@ -80,30 +74,36 @@ var func_query = function (json) {
         console.log('error:' + e)
     }
 }
-var query = function (str) {
+const query2 = str => {
     $('#collapsea').collapse('hide');
     a = str || document.getElementById("input_query").value;
-    var url = 'https://buguoheng.com/read.php' + (a == '' ? '' : '?a=' + a);
+    let url = 'https://buguoheng.com/read.php' + (a == '' ? '' : '?a=' + a);
     if (typeof str == 'object') {
         url = 'https://buguoheng.com/read.php' + '?id=' + localStorage.getItem('id') || '';
     }
-    //var callBack = function (json) { func_query(json); }
-    //HttpGet(url, callBack);
-    //window.history.replaceState(null, null, '/#'+a);
-    window.location.hash = url; 
+    var callBack = json => func_query(json);
+    HttpGet(url, callBack);
 }
-var query_onhashchange = function () {a=decodeURI(location.href).split('a=')[1]||''; HttpGet(location.hash.slice(1), function (json) { func_query(json); }); }
+const query = str => {
+    $('#collapsea').collapse('hide');
+    a = str || document.getElementById("input_query").value;
+    let url = 'https://buguoheng.com/read.php' + (a == '' ? '' : '?a=' + a);
+    if (typeof str == 'object') {
+        url = 'https://buguoheng.com/read.php' + '?id=' + localStorage.getItem('id') || '';
+    }
+    window.location.hash = url;
+}
+const query_onhashchange = () => { a = decodeURI(location.href).split('a=')[1] || ''; HttpGet(location.hash.slice(1), json => func_query(json)); }
 $(document).ready(function () {
-    console.log(location.hash.slice(1))
-    window.history.replaceState(null, null, '/#https://buguoheng.com/read.php');
+    window.history.replaceState(null, null, (decodeURI(location.href).split('a=')[1] == null) ? '/#https://buguoheng.com/read.php' : "/#" + location.hash.slice(1));
     query_onhashchange();
     window.addEventListener('hashchange', query_onhashchange, false);
 });
-var create = function (obj) {
+const create = obj => {
     $('#collapseb').collapse('hide');
     if (obj != undefined && obj.parentNode.id != undefined) {
-        var str_id = localStorage.getItem('id') || '';
-        var i = str_id.indexOf(obj.parentNode.id);
+        let str_id = localStorage.getItem('id') || '';
+        let i = str_id.indexOf(obj.parentNode.id);
         if (i >= 0) {
             localStorage.id = str_id.substring(0, i) + str_id.substring(i + obj.parentNode.id.length);
             query([]);
@@ -122,11 +122,11 @@ var create = function (obj) {
         b = document.getElementById("b").value;
     }
     if (a == '') { alert("please type a object") }
-    var url = 'https://buguoheng.com/create.php?a=' + a + '&b=' + JSON.stringify(b.split(","));
-    var callBack = function (create_id) {
+    let url = 'https://buguoheng.com/create.php?a=' + a + '&b=' + JSON.stringify(b.split(","));
+    let callBack = create_id => {
         if (create_id.length == 24) {
             localStorage.id += (',' + create_id);
-            query(a);
+            query2(a);
             if (a == 'id') {
                 $('#exampleModalLong').modal('show');
                 $(".modal-body")[0].innerHTML = create_id;
@@ -135,37 +135,37 @@ var create = function (obj) {
     }
     HttpGet(url, callBack);
 }
-document.getElementsByClassName("create")[0].addEventListener("keyup", function (event) { if (event.keyCode == 13) { create() } })
-document.getElementsByClassName("create")[1].addEventListener("keyup", function (event) { if (event.keyCode == 13) { create() } })
-document.getElementsByClassName("create")[1].addEventListener("keydown", function (event) { if (event.keyCode == 13) { event.preventDefault(); } })
+document.getElementsByClassName("create")[0].addEventListener("keyup", event => { if (event.keyCode == 13) { create() } })
+document.getElementsByClassName("create")[1].addEventListener("keyup", event => { if (event.keyCode == 13) { create() } })
+document.getElementsByClassName("create")[1].addEventListener("keydown", event => { if (event.keyCode == 13) { event.preventDefault(); } })
 
 
 $('#exampleModalLong').on('show.bs.modal', function () {
-    var readme = document.createElement("div");
+    let readme = document.createElement("div");
     $(readme).load("README.md", function () {
-        var converter = new showdown.Converter();
+        let converter = new showdown.Converter();
         $(".modal-body")[0].innerHTML = converter.makeHtml($(readme)[0].innerHTML);
     });
 })
-var rmcollapseb = function () { $('#collapseb').collapse('hide') }
+const rmcollapseb = () => $('#collapseb').collapse('hide');
 $('#collapseb').on('show.bs.collapse', function () {
-    document.getElementById('collapseb').addEventListener('click', function (e) { e.stopPropagation(); });
+    document.getElementById('collapseb').addEventListener('click', e => { e.stopPropagation(); });
     document.addEventListener('click', rmcollapseb, false);
 })
 $('#collapseb').on('hidden.bs.collapse', function () {
-    document.getElementById('collapseb').removeEventListener('click', function (e) { e.stopPropagation(); });
+    document.getElementById('collapseb').removeEventListener('click', e => { e.stopPropagation(); });
     document.removeEventListener('click', rmcollapseb, false);
 })
-var rmcollapsea = function () { $('#collapsea').collapse('hide') }
+const rmcollapsea = () => { $('#collapsea').collapse('hide') }
 $('#collapsea').on('show.bs.collapse', function () {
-    document.getElementById('collapsea').addEventListener('click', function (e) { e.stopPropagation(); });
+    document.getElementById('collapsea').addEventListener('click', e => { e.stopPropagation(); });
     document.addEventListener('click', rmcollapsea, false);
 })
 $('#collapsea').on('hidden.bs.collapse', function () {
-    document.getElementById('collapsea').removeEventListener('click', function (e) { e.stopPropagation(); });
+    document.getElementById('collapsea').removeEventListener('click', e => { e.stopPropagation(); });
     document.removeEventListener('click', rmcollapsea, false);
 })
 
 
-var totop = function () { $('body,html').animate({ scrollTop: '0px' }); }
-var tobottom = function () { $('body,html').animate({ scrollTop: $(".footer").offset().top }); }
+const totop = () => $('body,html').animate({ scrollTop: '0px' });
+const tobottom = () => $('body,html').animate({ scrollTop: $(".footer").offset().top }); 
