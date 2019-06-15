f2ffa1ab78155a7b337b88f1d0d083cbd2387bc0
vision <13253495869@163.com>
2019-06-15 15:03:27 +0800

    clean program

--- config.php
+++ config.php
@@ -1,5 +1,6 @@
 <?php
 $manager = new MongoDB\Driver\Manager("mongodb://localhost:27017");
+$db_name = 'bdb';
 $db_document = 'bdb.a';
 
 function FormitDollor($str)
@@ -10,7 +11,7 @@ function FormitDollor($str)
     if (is_object($str)) {
         $str = '\''.$str.'\'';
     }
-    $i = 0;
+    $i = 0; 
     while (strpos($str, '\$')) {
         $str = str_replace('\$', '$', $str);
         if ($i > 100) {
