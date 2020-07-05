6061d5c38191b1f601b7c8f15a8e6dff5cbba2db
bu <13253495869@163.com>
2020-07-05 14:14:59 +0800

    use justauth-demo

--- static/js/index.js
+++ static/js/index.js
@@ -38,15 +38,13 @@ const func_query = (json) => {
             let str_as = document.getElementById("as").innerHTML;
             document.getElementById("as").innerHTML = str_as.substring(0, str_as.indexOf(str) >= 0 ? str_as.indexOf(str) : 999);
             document.getElementById("as").innerHTML += str;
-        }
-        else {
+        } else {
             document.getElementById("as").innerHTML = '<a onclick="query()" style="margin-left:-15px;">&nbsp;&nbsp;&nbsp;</a><a class="float-right text-dark">' + getMyDate() + '</a></div>';
         }
         callBack2(json);
         document.getElementById("input_query").value = '';
         hide_id_edit();
-    }
-    catch (e) {
+    } catch (e) {
         console.log('error:' + e)
     }
 }
@@ -58,7 +56,7 @@ const callBack1 = (json) => {
     for (j in json) {
         div = document.createElement("div");
         aa = document.createElement("a");
-        aa.onclick = function () { query(this.innerHTML) };
+        aa.onclick = function() { query(this.innerHTML) };
         aa.innerHTML = json[j]['a'] || '';
         b = document.createElement("a");
         b.style = "font-size:80%";
@@ -71,7 +69,7 @@ const callBack1 = (json) => {
         star.src = 'static/svg/s.svg';
         star.width = "15";
         star.style = "margin:10px 5px;";
-        star.onclick = function () { create(this) };
+        star.onclick = function() { create(this) };
         div.className = "navbar-brand col-12 text-truncate border-bottom";
         div.id = json[j]['_id']['$oid'];
         div.appendChild(aa);
@@ -97,10 +95,10 @@ const callBack2 = (json) => {
         div = document.createElement("div");
         a = document.createElement("a");
         a.innerHTML = json[j]['a'] || '';
-        a.onclick = function () { query(this.innerHTML); }
+        a.onclick = function() { query(this.innerHTML); }
         let ahtml = a.innerHTML;
         b = document.createElement("a");
-        b.onclick = function () { query(ahtml); }
+        b.onclick = function() { query(ahtml); }
         b.innerHTML = (json[j]['b'] == null || json[j]['b'] == '' ? '' : json[j]['b'] + ' - ');
         a.style = b.innerHTML != '' ? "font-size:60%" : "";
         div.className = "style-1";
@@ -143,14 +141,12 @@ const skip = num => {
     console.log(url + skip);
     window.location.hash = url + skip;
 }
-const query_onhashchange = () => { a = decodeURI(location.href).split('a=')[1] || ''; HttpGet(location.hash.slice(1), json => func_query(json)); }
-$(document).ready(function () {
-    if (getQueryVariable('code')) {
-        let callBack = json => { if (json != null && json.data != null && json.data.username != null) alert("hello " + json.data.username) }
-        let url = "https://oauth.buguoheng.com/oauth/callback?";
-        //  url="http://localhost:81/oauth/callback?";
-        HttpGet(url + window.location.search.substring(1), callBack, true);
-    }
+const query_onhashchange = () => {
+    a = decodeURI(location.href).split('a=')[1] || '';
+    HttpGet(location.hash.slice(1), json => func_query(json));
+}
+$(document).ready(function() {
+    loginCallback();
     window.history.replaceState(null, null, (decodeURI(location.href).split('a=')[1] == null) ? '' : "/#" + location.hash.slice(1));
     query_onhashchange();
     window.addEventListener('hashchange', query_onhashchange, false);
@@ -168,12 +164,10 @@ const create = obj => {
         if (a == '') {
             a = obj.parentNode.childNodes[0].innerHTML;
             b = [];
-        }
-        else {
+        } else {
             b = obj.parentNode.childNodes[0].innerHTML;
         }
-    }
-    else {
+    } else {
         a = document.getElementById("a").value;
         b = document.getElementById("b").value;
     }
@@ -188,16 +182,14 @@ const create = obj => {
                 $('#exampleModalLong').modal('show');
                 $(".modal-body")[0].innerHTML = create_id;
             }
-        }
-        else {
+        } else {
             console.log('not right id,' + create_id)
         }
     }
     HttpGet(url, callBack);
 }
 const show_id_edit = () => {
-    if (document.getElementById('addid').style.display == 'block') { hide_id_edit(); }
-    else {
+    if (document.getElementById('addid').style.display == 'block') { hide_id_edit(); } else {
         tobottom();
         document.getElementById('addid').style.display = 'block';
         // document.getElementById('af').value = localStorage.id;
@@ -207,10 +199,20 @@ const show_id_edit = () => {
 const hide_id_edit = () => { document.getElementById('addid').style.display = 'none'; }
 const update_id = () => {
     localStorage.setItem(new Date().toLocaleString(), localStorage.id);
-    localStorage.id = document.getElementById('af').value; query([]);
+    localStorage.id = document.getElementById('af').value;
+    query([]);
 }
-const query_id = () => { document.body.innerHTML = "<button class='btn btn-primary' onclick='location.replace(location.href)'>刷新</a>"; for (i in localStorage) { if (i > '2019' && i < '2030' || i == "id") { document.body.innerHTML += "<h6>" + i + "</h6><p>" + localStorage[i] + "</p>"; } } document.body.innerHTML += "<br />"; }
-const order_id = () => { let ids = localStorage.getItem("id").split(','); let new_ids = ""; for (let i = 0; i < ids.length; i++) { if (ids[i].toString().length == 24) { new_ids += ids[i] + ','; } } document.getElementById('af').value = new_ids.substr(0, new_ids.length - 1); }
+const query_id = () => {
+    document.body.innerHTML = "<button class='btn btn-primary' onclick='location.replace(location.href)'>刷新</a>";
+    for (i in localStorage) { if (i > '2019' && i < '2030' || i == "id") { document.body.innerHTML += "<h6>" + i + "</h6><p>" + localStorage[i] + "</p>"; } }
+    document.body.innerHTML += "<br />";
+}
+const order_id = () => {
+    let ids = localStorage.getItem("id").split(',');
+    let new_ids = "";
+    for (let i = 0; i < ids.length; i++) { if (ids[i].toString().length == 24) { new_ids += ids[i] + ','; } }
+    document.getElementById('af').value = new_ids.substr(0, new_ids.length - 1);
+}
 document.getElementById("input_query").addEventListener("keyup", event => { if (event.keyCode == 13) { query(document.getElementById("input_query").value) } })
 document.getElementsByClassName("create")[0].addEventListener("keyup", event => { if (event.keyCode == 13) { create() } })
 document.getElementsByClassName("create")[1].addEventListener("keyup", event => { if (event.keyCode == 13) { create() } })
@@ -220,15 +222,43 @@ document.getElementById("div_card").style.minHeight = window.innerHeight - 90 + 
 // keyboard type 'enter' to open input_query,need auto remove EventListener
 let i = 0;
 let enter_keycode = [69, 78, 84, 69, 82];
-const quick_open = event => { if (event.keyCode == enter_keycode[i]) { if (i++ == 4) { $('#collapseb').collapse('show'); $('#a').focus(); i = 0; } } else { i = 0; } };
+const quick_open = event => {
+    if (event.keyCode == enter_keycode[i]) {
+        if (i++ == 4) {
+            $('#collapseb').collapse('show');
+            $('#a').focus();
+            i = 0;
+        }
+    } else { i = 0; }
+};
 document.addEventListener('keyup', quick_open, true);
 
 // show README.md
-$('#exampleModalLong').on('show.bs.modal', function () { let readme = document.createElement("div"); $(readme).load("README.md", function () { let converter = new showdown.Converter(); $(".modal-body")[0].innerHTML = converter.makeHtml($(readme)[0].innerHTML); }); })
-const rmcollapseb = () => $('#collapseb').collapse('hide'); $('#collapseb').on('show.bs.collapse', function () { document.getElementById('collapseb').addEventListener('click', e => { e.stopPropagation(); }); document.addEventListener('click', rmcollapseb, false); })
-$('#collapseb').on('hidden.bs.collapse', function () { document.getElementById('collapseb').removeEventListener('click', e => { e.stopPropagation(); }); document.removeEventListener('click', rmcollapseb, false); })
-const rmcollapsea = () => { $('#collapsea').collapse('hide') }; $('#collapsea').on('show.bs.collapse', function () { document.getElementById('collapsea').addEventListener('click', e => { e.stopPropagation(); }); document.addEventListener('click', rmcollapsea, false); })
-$('#collapsea').on('hidden.bs.collapse', function () { document.getElementById('collapsea').removeEventListener('click', e => { e.stopPropagation(); }); document.removeEventListener('click', rmcollapsea, false); })
+$('#exampleModalLong').on('show.bs.modal', function() {
+    let readme = document.createElement("div");
+    $(readme).load("README.md", function() {
+        let converter = new showdown.Converter();
+        $(".modal-body")[0].innerHTML = converter.makeHtml($(readme)[0].innerHTML);
+    });
+})
+const rmcollapseb = () => $('#collapseb').collapse('hide');
+$('#collapseb').on('show.bs.collapse', function() {
+    document.getElementById('collapseb').addEventListener('click', e => { e.stopPropagation(); });
+    document.addEventListener('click', rmcollapseb, false);
+})
+$('#collapseb').on('hidden.bs.collapse', function() {
+    document.getElementById('collapseb').removeEventListener('click', e => { e.stopPropagation(); });
+    document.removeEventListener('click', rmcollapseb, false);
+})
+const rmcollapsea = () => { $('#collapsea').collapse('hide') };
+$('#collapsea').on('show.bs.collapse', function() {
+    document.getElementById('collapsea').addEventListener('click', e => { e.stopPropagation(); });
+    document.addEventListener('click', rmcollapsea, false);
+})
+$('#collapsea').on('hidden.bs.collapse', function() {
+    document.getElementById('collapsea').removeEventListener('click', e => { e.stopPropagation(); });
+    document.removeEventListener('click', rmcollapsea, false);
+})
 
 //change page position
 const totop = () => $('body,html').animate({ scrollTop: '0px' });
@@ -238,7 +268,14 @@ const tobottom = () => $('body,html').animate({ scrollTop: $(".footer").offset()
 // document.getElementById('div_skip').innerHTML = '<button class="btn btn-light btn-sm" onclick = "skip(0)" > 1</button> <button class="btn btn-light btn-sm" onclick="skip(1)">2</button> <button class="btn btn-light btn-sm" onclick="skip(2)">3</button> <button class="btn btn-light btn-sm" onclick="skip(3)">4</button> <button class="btn btn-light btn-sm" onclick="skip(4)">5</button> <button class="btn btn-light btn-sm" onclick="skip(5)">6</button> <button class="btn btn-light btn-sm" onclick="skip(6)">7</button> <button class="btn btn-light btn-sm" onclick="skip(7)">8</button> <input class="btn btn-sm border" border-radius="5px" type="text" id="skip" size="2"> <button class="btn btn-light btn-sm" onclick="skip(document.getElementById("skip").value)">skip</button>' + document.getElementById('div_skip').innerHTML;
 
 const login = () => {
-    window.location.href = 'https://oauth.buguoheng.com/oauth/render';
+    window.location.href = 'https://oauth.buguoheng.com/oauth/render/github';
+}
+const loginCallback = () => {
+    if (getQueryVariable('code')) {
+        let callBack = json => { if (json != null && json.data != null && json.data.username != null) alert("hello " + json.data.username) }
+        let url = "https://oauth.buguoheng.com/oauth/callback/github";
+        HttpGet(url + window.location.search.substring(1), callBack, true);
+    }
 }
 const getQueryVariable = (variable) => {
     let query = window.location.search.substring(1);
