08db22a7c829c02007cc40c439e7f450b5ab4fff
bghuan <13253495869@163.com>
2020-09-07 20:36:21 +0800

    change websocket

--- static/js/websocket.js
+++ static/js/websocket.js
@@ -1,14 +1,17 @@
 var WebSocketServer = require('ws').Server
 wss = new WebSocketServer({ port: 8091 });
 
+let lastedMessage
+let clientList = []
+
 wss.on('connection', function (client) {
+    if (lastedMessage) client.send(lastedMessage)
     clientList.push(client);
     client.on('message', function (message) {
+        lastedMessage = message
         broadcast(message, client);
     });
 });
-
-let clientList = [];
 
 function broadcast(message, client) {
     for (var i = 0; i < clientList.length; i++) {
@@ -18,4 +21,4 @@ function broadcast(message, client) {
     }
 }
 
-setInterval(() => broadcast('', null), 60000)
+setInterval(() => { broadcast('', null); lastedMessage = null }, 60000)
