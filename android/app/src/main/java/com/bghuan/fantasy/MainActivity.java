1d2e29105db294559a3e77cc957ea206c371a2c7
bu <13253495869@163.com>
2020-09-30 15:31:56 +0800

    can not add loading page for android

--- android/app/src/main/java/com/bghuan/fantasy/MainActivity.java
+++ android/app/src/main/java/com/bghuan/fantasy/MainActivity.java
@@ -1,8 +1,10 @@
 package com.bghuan.fantasy;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.View;
@@ -15,33 +17,24 @@ public class MainActivity extends AppCompatActivity {
     private WebView webView;
 
     @SuppressLint("SetJavaScriptEnabled")
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         webView = findViewById(R.id.fullscreen_content);
-        webView.post(mHidePart2Runnable);
-
         webView.loadUrl("https://www.buguoheng.com"); // 加载url
 
-        webView.setWebViewClient(webViewClient);
-
         WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true); // 允许使用 js
+        webSettings.setDomStorageEnabled(true); // 允许使用 LocalStorage
 
-        webSettings.setJavaScriptEnabled(true); // 允许使用js
-        webSettings.setDomStorageEnabled(true); // LocalStroage
+        webView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR); // 状态栏文字黑色
+
+        webView.setWebViewClient(webViewClient); // webView监听url跳转事件
+        webView.setVisibility(View.VISIBLE);
     }
 
-    //    设置状态栏文字颜色为黑
-    private final Runnable mHidePart2Runnable = new Runnable() {
-        @SuppressLint("InlinedApi")
-        @Override
-        public void run() {
-            webView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
-        }
-    };
-
-    //    webView监听url跳转事件
     private WebViewClient webViewClient = new WebViewClient() {
         @Override
         public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
