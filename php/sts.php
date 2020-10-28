ab02296da99ed0e8f77705c3e123a1b5cae179d0
bghuan <13253495869@163.com>
2020-10-28 20:12:27 +0800

    add 近物远视 fantasy

---
+++ php/sts.php
@@ -0,0 +1,40 @@
+<?php
+
+require __DIR__ . '/vendor/autoload.php';
+
+header('Access-Control-Allow-Origin:*');
+header('Content-type: application/json; charset=utf-8');
+include 'config.php';
+
+use AlibabaCloud\Client\AlibabaCloud;
+use AlibabaCloud\Client\Exception\ClientException;
+use AlibabaCloud\Client\Exception\ServerException;
+
+//构建一个阿里云客户端，用于发起请求。
+//构建阿里云客户端时需要设置AccessKey ID和AccessKey Secret。
+AlibabaCloud::accessKeyClient($accessKeyId2, $accessKeySecret2)
+                        ->regionId($regionId)
+                        ->asDefaultClient();
+//设置参数，发起请求。
+try {
+    $result = AlibabaCloud::rpc()
+                          ->product('Sts')
+                          ->scheme('https') // https | http
+                          ->version('2015-04-01')
+                          ->action('AssumeRole')
+                          ->method('POST')
+                          ->host('sts.aliyuncs.com')
+                          ->options([
+                                        'query' => [
+                                          'RegionId' => $regionId,
+                                          'RoleArn' => $roleArn,
+                                          'RoleSessionName' => $roleSessionName,
+                                        ],
+                                    ])
+                          ->request();
+    print_r(json_encode($result->toArray()));
+} catch (ClientException $e) {
+    echo $e->getErrorMessage() . PHP_EOL;
+} catch (ServerException $e) {
+    echo $e->getErrorMessage() . PHP_EOL;
+}
