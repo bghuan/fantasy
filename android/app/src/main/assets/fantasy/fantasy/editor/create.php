204432b4dd3ab9cac68ce45dfef41eee9f12347b
ft-bugh <bugh@fintechgp.com>
2022-06-27 11:47:20 +0800

    clean other project

--- android/app/src/main/assets/fantasy/fantasy/editor/create.php
+++
@@ -1,9 +0,0 @@
-<?php
-header('Access-Control-Allow-Origin:*');
-header('Content-type: application/json; charset=utf-8');
-// header("Content-Encoding: gzip");
-include 'config.php';
-
-$result = $redis->set($key, $content);
-echo $result;
-$redis->set($is_backup, false);
