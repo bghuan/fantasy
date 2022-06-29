732e4c26751dd6d6755e54d93ef4de433e587e18
ft-bugh <bugh@fintechgp.com>
2022-06-29 11:25:54 +0800

    save.php add muti key value support

--- php/save.php
+++ php/save.php
@@ -3,10 +3,20 @@ header('Access-Control-Allow-Origin:*');
 header('Content-type: text/html; charset=utf-8');
 include 'config.php';
 
-$key = $_GET["key"];
-$value = $_GET["value"];
-if(empty($value)){
-echo $redis->get($key);
-exit;
+$namespace = $_POST["namespace"] ? $_POST["namespace"] : $_GET["namespace"];
+$prix = $_SERVER['PHP_SELF'] . '.' . $namespace . '.';
+$keyvalue = $_POST ? $_POST : $_GET;
+$array;
+
+foreach ($keyvalue as $key => $value) {
+    $my_key = $prix . $key;
+    if (empty($value)) {
+        $value = $redis->get($my_key);
+        $value = ($value == false && !$redis->exists($my_key)) ? null : $value;
+    } else {
+        $redis->set($my_key, $value);
+    }
+    $array[$key] = $value;
 }
-echo $redis->set($key,$value);
+
+echo json_encode2($array);
